1cbab092f685a0fddc9f41f07b9188fec88cce4a
guru28 <sgurumoorthy.28@gmail.com>
2017-08-20 22:18:58 +0530

    error message is fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+	
+  validates :email, 
+    presence: true, 
+    format: {with: VALID_EMAIL , message: "invalid"}
+
   has_many :incomes
   has_many :expences
 end

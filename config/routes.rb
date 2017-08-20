631dd67876888247164fff66348f1ffb6261f0ab
guru28 <sgurumoorthy.28@gmail.com>
2017-08-20 17:37:27 +0530

    error in pdf export is fixed and styles is added

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   end 
   devise_for :users
 
+  get '/incomes/reports.pdf', to: "incomes#reports", as: "download_reports" 
   root "incomes#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
